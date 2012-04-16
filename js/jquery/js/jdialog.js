315c19480c318118ab7a1594c1001f0a347a4055
panther <panther@panther.(none)>
2012-04-15 22:29:04 -0300

    nuevo form productos

--- js/jquery/js/jdialog.js
+++ js/jquery/js/jdialog.js
@@ -15,3 +15,15 @@ $(document).ready(function(){
 			});
 
 		});
+
+$(document).ready(function() {
+			$("#gallery_output img").not(":first").hide();
+ 
+			$("#gallery a").click(function() {
+				if ( $("#" + this.rel).is(":hidden") ) {
+					$("#gallery_output img").slideUp();
+					$("#" + this.rel).slideDown();
+				}
+			});
+		});
+ 
