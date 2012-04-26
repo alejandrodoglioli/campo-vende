e7d847baa9a1c35fe048d34ad6e6b705a34c8bbc
Alejandro Doglioli <alejandrodoglioli@gmail.com>
2012-04-25 22:16:56 -0300

    usar remotescriptong para validar email

--- js/jquery/js/olvidepass.js
+++ js/jquery/js/olvidepass.js
@@ -1,8 +1,12 @@
+
+
 $(function() {
 		// a workaround for a flaw in the demo system (http://dev.jqueryui.com/ticket/4375), ignore!
 		$( "#dialog:ui-dialog" ).dialog( "destroy" );
+		var bValid = true;		
+		var emailexiste=false;
 		
-		var email = $( "#email" ),
+		var email = $( "#email_rec" ),
 		 	
 			allFields = $( [] ).add( email ),
 			captcha = $( "#captcha" ),
@@ -68,12 +72,13 @@ $(function() {
 				return false;
 			}
 			else{
-				updateTips( "se mete por aca");
 				return true;
 			}
 		}
 		
-		function checkLongPassword(p){
+
+
+	   function checkLongPassword(p){
 			if(p < 6){
 				o.addClass( "ui-state-error" );
 				updateTips( "La Password debe contener al menos 6 caracteres");
@@ -84,23 +89,29 @@ $(function() {
 			}
 		}
 		
-
+		
+		function showInvalidEmail(o) {
+			 o.addClass( "ui-state-error" );
+			 updateTips( o.val()+" no se corresponde a un usuario válido.");
+			 return false;
+		}
 		
 		$( "#dialog-form-pass" ).dialog({
 			autoOpen: false,
 			height: 550,
 			width: 500,
-			modal: true,
+			modal: false,
 			
 			buttons: {
 				"Enviar": function() {
-					var bValid = true;
+					
 					allFields.removeClass( "ui-state-error" );
 						
 					bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "La direccion de 'Email' es invalida" );
-			//		bValid = bValid && checkCaptcha( captcha );
-
-				
+					
+					validar_email(email.val());
+					bValid = bValid && checkCaptcha( captcha );
+					alert($( "#emailExiste" ).val());
 					if ( bValid ) {
 						$( "#users tbody" ).append( "<tr>" +
 							"<td>" + email.val() + "</td>" +
@@ -108,7 +119,11 @@ $(function() {
 						
 						$( "#formOlvidePass" ).submit();
 						$( this ).dialog( "close" );
+					}else if(	  document.getElementById("emailExiste").innerHTML==0){
+						
+						showInvalidEmail(email);
 					}
+					
 				},
 				"Cancelar": function() {
 					$( this ).dialog( "close" );
@@ -128,3 +143,5 @@ $(function() {
 		
 	
 	});
+
+
