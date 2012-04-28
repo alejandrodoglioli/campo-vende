00375caa1828fff37a5a6d75e19daa580ddaf6eb
Alejandro Doglioli <alejandrodoglioli@gmail.com>
2012-04-28 15:59:47 -0300

    cabios login

---
+++ modulos/usuarios_sistema/include/include.remotescriptinglogin.php
@@ -0,0 +1,28 @@
+<?php
+
+include("../../../include/class.Template.php");
+include("../../../include/config.php");
+include("../../../include/conexion.php");
+require("../../../admin/include/jsrsServer.php.inc");
+
+jsrsDispatch( "recuperarEmail" );
+
+function recuperarEmail($email){
+
+	global $tof_usuarios_sistema;
+	
+	$result=mysql_query("select u.password from ".$tof_usuarios_sistema." u where u.email='".$email."'");
+	$row=mysql_fetch_array($result);
+    $out = array();
+
+    
+	if (mysql_num_rows($result)<=0){
+		return 0;
+	}else{
+		return $row['password'];
+	}
+	
+}
+		
+?>
+
